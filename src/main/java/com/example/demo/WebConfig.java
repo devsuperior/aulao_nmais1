3e2ca4836051e3f3b19b590b16380c66637d90bb
Nelio Alves <acenelio@gmail.com>
2024-11-06 11:50:07 -0300

    Remove Serializing PageImpl warning

---
+++ src/main/java/com/example/demo/WebConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(
+        pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
+public class WebConfig {
+}
